23be6957de985902bbc721b10fd6bffe936dcfb7
Shradha1824 <shradhabj1824@gmail.com>
2021-04-30 00:51:11 -0700

    ADDED: using array

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-
+#calculete how much work completed in how much days
 #Constants
+
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_HALF_TIME=2
 EMP_RATE_PER_HR=20
@@ -10,6 +11,7 @@ MAX_WORKING_HOURS=20
 #varials
 totalworkingHours=0
 totalworkingDays=0
+totalEmpHr=0
 #whileloop
 #< -----> -lt / <= ----- le / > -gt / >= -ge / === ----> -eq
 function getWorkHours(){
@@ -25,14 +27,19 @@ function getWorkHours(){
 while [ $totalworkingDays -lt $NO_OF_WORKING_DAYS ] && [ $totalworkingHours -lt $MAX_WORKING_HOURS ]
 
 do
-       empCheck=$(( RANDOM%3 ))
        totalworkingDays=$(( $totalworkingDays + 1 ))
-       empHrs="$( getWorkHours $empCheck )"
+       empHrs="$( getWorkHours $((RANDOM%3)) )"
        totalworkingHours=$(( $totalworkingHours + $empHrs ))
-
+       dailyWage[$totalworkingDays]=$(( empHrs * EMP_RATE_PER_HR ))
+       #dailyWage[1]=4*20=80
 done
-salary=$(( $EMP_RATE_PER_HR * $totalworkingHours ))
+totalsalary=$(( $EMP_RATE_PER_HR * $totalworkingHours ))
+#echo ${dailyWage[*]}
+#echo ${!dailyWage[*]}
+#echo ${#dailyWage[*]}
+#echo ${dailyWage[0]}
 #salary=$(( EMP_RATE_PER_HR * empHrs ))
+
 
 
 
