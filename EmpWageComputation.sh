841d4693a521f8f9ba2b193d5a51930f8521ef20
Shradha1824 <shradhabj1824@gmail.com>
2021-04-30 01:03:12 -0700

    ADDED: for total salary

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-#calculete how much work completed in how much days
+
+#calculete total salary for total working hours
 #Constants
 
 IS_PRESENT_FULL_TIME=1
@@ -11,34 +12,28 @@ MAX_WORKING_HOURS=20
 #varials
 totalworkingHours=0
 totalworkingDays=0
-totalEmpHr=0
 #whileloop
 #< -----> -lt / <= ----- le / > -gt / >= -ge / === ----> -eq
-function getWorkHours(){
-          local empCheck=$1
-          local empHrs=0
-           case $empCheck in
-                 $IS_PRESENT_FULL_TIME) empHrs=8 ;;
-                 $IS_PRESENT_HALF_TIME) empHrs=4 ;;
-                 *) empHrs=0 ;;
-         esac
-         echo $empHrs
-}
+
 while [ $totalworkingDays -lt $NO_OF_WORKING_DAYS ] && [ $totalworkingHours -lt $MAX_WORKING_HOURS ]
 
 do
+       employeeCheck=$(( RANDOM%3 ))
+
        totalworkingDays=$(( $totalworkingDays + 1 ))
-       empHrs="$( getWorkHours $((RANDOM%3)) )"
+       #condition
+       case $employeeCheck in
+                 $IS_PRESENT_FULL_TIME) empHrs=8 ;;
+                 $IS_PRESENT_HALF_TIME) empHrs=4 ;;
+                 *) empHrs=0 ;;
+       esac
        totalworkingHours=$(( $totalworkingHours + $empHrs ))
-       dailyWage[$totalworkingDays]=$(( empHrs * EMP_RATE_PER_HR ))
-       #dailyWage[1]=4*20=80
+
 done
-totalsalary=$(( $EMP_RATE_PER_HR * $totalworkingHours ))
-#echo ${dailyWage[*]}
-#echo ${!dailyWage[*]}
-#echo ${#dailyWage[*]}
-#echo ${dailyWage[0]}
+salary=$(( $EMP_RATE_PER_HR * $totalworkingHours ))
 #salary=$(( EMP_RATE_PER_HR * empHrs ))
+
+
 
 
 
