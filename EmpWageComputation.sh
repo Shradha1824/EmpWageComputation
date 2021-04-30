fded00c7cab79c023e1dcbeaef38033d76af711c
Shradha1824 <shradhabj1824@gmail.com>
2021-04-29 21:56:48 -0700

    ADDED:EmpWageComputation.sh to add part time Employee & Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,19 +1,23 @@
 #! /bin/bash
 
 #Constants
-IS_PRESENT=1
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_HALF_TIME=2
 EMP_RATE_PER_HR=20
 
 #Variables
-empCheck=$(( RANDOM%2 ))
+empCheck=$(( RANDOM%3 ))
 
 #Selection
-if [ $empCheck -eq $IS_PRESENT ]
+if [ $empCheck -eq $IS_PRESENT_FULL_TIME ]
 then
       empHrs=8
+elif [ $empCheck -eq $IS_PRESENT_HALF_TIME ]
+then
+      empHrs=4
 else
       empHrs=0
 fi
-salary=$(( EMP_RATE_PER_HR * empHrs ))
+
 
 
