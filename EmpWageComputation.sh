b549aebf3ed88f4aa22f010b772fbfde605045c4
Shradha1824 <shradhabj1824@gmail.com>
2021-04-29 22:10:50 -0700

    ADDED:for calculating Wages for a month

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,18 +1,30 @@
 #! /bin/bash
 
 #Constants
-
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_HALF_TIME=2
-EMP_RATE_PER_HR=20
+WAGE_PER_HR=20
+MAX_WORKING_DAYS=20
 
-empCheck=$(( RANDOM %3 ))
-case $empCheck in
-       $IS_PRESENT_FULL_TIME) empHrs=8;;
-       $IS_PRESENT_HALF_TIME) empHrs=4;;
-       *) empHrs=0;;
-esac
-salary=$(( EMP_RATE_PER_HR * empHrs ))
+#variables
+totalworkingHours=0
+totalworkingDays=0
+while [ $totalworkingDays -ne $MAX_WORKING_DAYS ]
+do
+       employeeCheck=$((RANDOM%3))
+       ((totalworkingDays++))
+       case $employeeCheck in
+              $IS_PRESENT_FULL_TIME)
+                     empHrs=8 ;;
+              $IS_PRESENT_HALF_TIME)
+                     empHrs=4 ;;
+                                  *)
+                     empHrs=0 ;;
+       esac
+       totalworkingHours=$(( totalworkingHours + empHrs ))
+done
+salary=$(( totalworkingHours * WAGE_PER_HR ))
+
 
 
 
