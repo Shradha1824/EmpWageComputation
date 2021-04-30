18f933f09abfcd12582cb9885e7cfda1e9e4b7fc
Shradha1824 <shradhabj1824@gmail.com>
2021-04-29 22:04:45 -0700

    ADDED:solving using Switch case statement

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,23 +1,20 @@
 #! /bin/bash
 
 #Constants
+
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_HALF_TIME=2
 EMP_RATE_PER_HR=20
 
-#Variables
-empCheck=$(( RANDOM%3 ))
+empCheck=$(( RANDOM %3 ))
+case $empCheck in
+       $IS_PRESENT_FULL_TIME) empHrs=8;;
+       $IS_PRESENT_HALF_TIME) empHrs=4;;
+       *) empHrs=0;;
+esac
+salary=$(( EMP_RATE_PER_HR * empHrs ))
 
-#Selection
-if [ $empCheck -eq $IS_PRESENT_FULL_TIME ]
-then
-      empHrs=8
-elif [ $empCheck -eq $IS_PRESENT_HALF_TIME ]
-then
-      empHrs=4
-else
-      empHrs=0
-fi
+
 
 
 
